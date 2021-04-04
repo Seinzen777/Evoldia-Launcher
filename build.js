b601be82fe74ad08cae9e95cd655968f434878fb
Seinzen <tiallan.ytb@gmail.com>
2021-04-04 04:12:07 -0400

    Update
    Test Update

--- build.js
+++ build.js
@@ -19,9 +19,9 @@ builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
         appId: 'evoldianetwork',
-        productName: 'Helios Launcher',
+        productName: 'Evoldia Network Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2018-2021 Evoldia Network',
         directories: {
             buildResources: 'build',
             output: 'dist'
